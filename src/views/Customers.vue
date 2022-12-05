815acfd52d2c9739299322291e0c02b08010c3bc
Ungerm23 <mark.unger@student.htw-berlin.de>
2022-12-05 17:32:49 +0100

    init and added HelloWorld and Navbar vue

---
+++ src/views/Customers.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome to Customers</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Customers'
+}
+</script>
+
+<style scoped>
+
+</style>
